6bca5d4eda0102fdf0705276055257048f8b739e
caiyonglong <caiyonglong@live.com>
2017-12-06 23:16:15 +0800

    update wallpaper res

--- app/src/main/java/com/dragon/wallpaperapp/ui/activity/WallpaperDisplayActivity.kt
+++ app/src/main/java/com/dragon/wallpaperapp/ui/activity/WallpaperDisplayActivity.kt
@@ -1,13 +1,18 @@
 package com.dragon.wallpaperapp.ui.activity
 
+import android.app.WallpaperManager
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
 import com.dragon.wallpaperapp.R
 import com.dragon.wallpaperapp.api.GlideApp
 import kotlinx.android.synthetic.main.activity_wallpaper.*
+
 
 /**
  * Created by D22434 on 2017/12/6.
@@ -30,10 +35,19 @@ class WallpaperDisplayActivity : AppCompatActivity() {
                 .diskCacheStrategy(DiskCacheStrategy.NONE)
                 .transition(DrawableTransitionOptions.withCrossFade())
                 .into(cropView)
-
         btn_set_wallpaper.setOnClickListener { v ->
+            val mWallManager = WallpaperManager.getInstance(this)
+            GlideApp.with(this)
+                    .asBitmap()
+                    .load(url)
+                    .into(object : SimpleTarget<Bitmap>() {
+                        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>) {
+                            mWallManager.setBitmap(resource)
+                        }
+                    })
             Toast.makeText(this, "设置壁纸中", Toast.LENGTH_SHORT).show()
         }
     }
+
 
 }
